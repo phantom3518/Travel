acce57e1440a754478ab93625404a7e4b84fce5d
phantom3518 <1085540763@qq.com>
2019-08-07 14:16:53 +0800

    异步组件

--- src/router.js
+++ src/router.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home/Home.vue'
-import City from './views/city/City.vue'
-import Detail from './views/detail/Detail.vue'
 
 Vue.use(Router)
 
@@ -12,17 +9,18 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/views/home/Home.vue')
   },
   {
     path: '/city',
     name: 'City',
-    component: City
+    component: () => import('@/views/city/City.vue')
+
   },
   {
     path: '/detail/:id',
     name: 'Detail',
-    component: Detail
+    component: () => import('@/views/detail/Detail.vue')
   }],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
